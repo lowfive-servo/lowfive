8241c45c193e3972d361ae99ca5fd58194233edc
erneyja <joshua.erney@gmail.com>
2015-09-02 08:41:16 -0400

    commiting for save

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
+import configparser
 import json
 import unittest
-from tidycommenter import parse_payload
+from tidycommenter import parse_payload, TravisAPIProvider
 
 class TestJSONParse(unittest.TestCase):
 	def setUp(self):
@@ -9,6 +10,16 @@ class TestJSONParse(unittest.TestCase):
 
 	def test_parse_payload(self):
 		self.assertEqual("74856035", parse_payload(self.payload))
+
+class TestTravisIntegration(unittest.TestCase):
+	def setUp(self):
+		config = configparser.RawConfigParser()
+		config.read('config')
+		github_token = config['github']['token']
+		self.travis_api = TravisAPIProvider(github_token)
+
+	def test_something_here(self):
+		print(self.travis_api.get_token())
 
 if __name__ == '__main__':
 	unittest.main()
